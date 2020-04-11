da3eaf21791cb73174a321081ddb83808b8b838f
Thomas Pope <tpope0928@gmail.com>
2020-04-11 00:23:14 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,7 @@
-def show
-  sighting = Sighting.find_by(id: params[:id])
-  render json: sighting, include: [:bird, :location]
+def index
+  sightings = Sighting.all
+  render json: sightings.to_json(include: [:bird, :location])
 end
+
+
+
